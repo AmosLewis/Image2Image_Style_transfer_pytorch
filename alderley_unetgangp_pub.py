e4463a48c33a3fdfc50514e36da6cbca0b7211cf
jblamare <jblamare@yahoo.fr>
2018-05-05 15:41:39 -0400

    As many layers as in the paper

---
+++ alderley_unetgangp_pub.py
@@ -0,0 +1,337 @@
+import argparse
+import math
+import os
+import sys
+import numpy as np
+from PIL import Image
+
+import torch
+from inferno.trainers.basic import Trainer
+from torch import nn
+from torch.autograd import Variable
+from torch.optim import Adam
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data.dataset import Dataset
+from torchvision import datasets
+from torchvision import transforms
+from inferno.trainers.callbacks.base import Callback
+
+from gan_utils import Reshape, format_images
+from gan_utils import save_args, initializer
+from wgan_loss import WGANDiscriminatorLoss, WGANGeneratorLoss
+from unet_pub import UnetUpsample
+
+
+class AlderleyWrapper(Dataset):
+    def __init__(self):
+        super(AlderleyWrapper, self).__init__()
+        path = "data/alderley/alderley.npy"
+        data = np.transpose(np.load(path), (1, 0, 2, 3, 4))
+        self.days = torch.Tensor(data[0][:-1])
+        self.nights = torch.Tensor(data[1][:-1])
+        self.valid_day = torch.Tensor(data[0][-1:])
+        self.valid_night = torch.Tensor(data[1][-1:])
+        print(self.days[0].mean(), self.nights[0].mean())
+        print("Alderley dataset loaded")
+
+    def __len__(self):
+        return len(self.days)
+
+    def __getitem__(self, item):
+        # print(item)
+        x, y = self.nights[item], self.days[item]
+        # print(x.size())
+        # print(y.size())
+        return x, y, y
+
+    def valid_pictures(self):
+        # print(item)
+        x, y = self.valid_day, self.valid_night
+        # print(x.size())
+        # print(y.size())
+        return x, y
+
+
+def alderley_cgan_data_loader(args, dataset):
+    # Create DataLoader for Alderley
+    kwargs = {'num_workers': 2, 'pin_memory': True} if args.cuda else {}
+    train_loader = DataLoader(
+        dataset,
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    return train_loader
+
+
+class CDiscriminatorNetwork(nn.Module):
+    # Network for discrimination
+    # Input is (N, 6, 256, 512)
+    def __init__(self, args):
+        super(CDiscriminatorNetwork, self).__init__()
+        self.trunk = nn.Sequential(*[m for m in [
+            nn.Conv2d(6, 64, kernel_size=4, stride=2, padding=1),  # N, 64, 128, 256
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # N, 128, 64, 128
+            nn.InstanceNorm2d(128) if args.discriminator_instancenorm else None,
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # N, 256, 32, 64
+            nn.InstanceNorm2d(256) if args.discriminator_instancenorm else None,
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 16, 32
+            nn.InstanceNorm2d(512) if args.discriminator_instancenorm else None,
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 8, 16
+            nn.InstanceNorm2d(512) if args.discriminator_instancenorm else None,
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 4, 8
+            nn.InstanceNorm2d(512) if args.discriminator_instancenorm else None,
+            nn.LeakyReLU(negative_slope=0.2),
+            Reshape(-1, 512 * 4 * 8),  # N, 512 * 8 * 4
+            nn.Linear(512 * 4 * 8, 1024),  # N, 1024
+            nn.LeakyReLU(),
+            nn.Linear(1024, 1),  # N, 1
+            Reshape(-1)] if m is not None])  # N
+        # nn.Conv2d(512, 1, kernel_size=(8, 16), stride=(4, 8), padding=(2, 4)),  # N, 1, 1, 1
+        # nn.Sigmoid(),
+        # Reshape(-1)] if m is not None])  # N
+
+    def forward(self, x, y):
+        h = torch.cat((x, y), dim=1)
+
+        h = self.trunk(h)
+        return h
+
+
+class CGANModel(nn.Module):
+    # GAN containing generator and discriminator
+    def __init__(self, args, discriminator, generator):
+        super(CGANModel, self).__init__()
+        self.discriminator = discriminator
+        self.generator = generator
+
+        self._state_hooks = {}  # used by inferno for logging
+        self.apply(initializer)  # initialize the parameters
+
+    def generate(self, y):
+        # Generate fake images from input
+
+        xfake = self.generator(y)
+        # Save images for later
+        self._state_hooks['xfake'] = xfake
+        self._state_hooks['y'] = y
+        self._state_hooks['generated_images'] = format_images(xfake)  # log the generated images
+        return xfake
+
+    def discriminate(self, x, y):
+        # Run discriminator on an input
+        return self.discriminator(x, y)
+
+    def y_fake(self, xfake, y):
+        # Run discriminator on generated images
+        yfake = self.discriminate(xfake, y)
+        return yfake
+
+    def y_real(self, xreal, y):
+        # Run discriminator on real images
+        yreal = self.discriminate(xreal, y)
+        # Save images for later
+        self._state_hooks['xreal'] = xreal
+        self._state_hooks['real_images'] = format_images(xreal)
+        return yreal
+
+    def l1_loss(self, xreal, xfake):
+        loss = torch.abs(xfake - xreal)
+        loss = loss.sum() / xreal.shape[0]
+        return loss
+
+    def forward(self, xreal, y):
+        # Calculate and return y_real and y_fake and the L1 loss
+        xfake = self.generate(y)
+        return self.y_real(xreal, y), self.y_fake(xfake, y)
+
+
+class CWGANDiscriminatorLoss(WGANDiscriminatorLoss):
+    def discriminate(self, xmix):
+        y = self.model._state_hooks['y']
+        return self.model.discriminate(xmix, y)
+
+
+class CGenerateDataCallback(Callback):
+    # Callback saves generated images to a folder
+
+    def __init__(self, args, dataset, gridsize=1):
+        super(CGenerateDataCallback, self).__init__()
+        self.count = 0  # iteration counter
+        self.image_count = 0  # image counter
+        self.frequency = args.image_frequency
+        self.gridsize = gridsize
+        self.dataset = dataset
+        self.y = self.dataset.valid_night
+        self.xreal = self.dataset.valid_day
+
+    def end_of_training_iteration(self, **_):
+        # Check if it is time to generate images
+        if(self.image_count == 0 and self.count == 0):
+            self.save_images(source=True)
+            self.save_images(real=True)
+        self.count += 1
+        if self.count > self.frequency:
+            self.save_images()
+            self.count = 0
+
+    def generate(self):
+        # Set eval, generate, then set back to train
+        self.trainer.model.eval()
+        y = Variable(self.y)
+        if self.trainer.is_cuda():
+            y = y.cuda()
+        generated = self.trainer.model.generate(y)
+        self.trainer.model.train()
+        return generated
+
+    def save_images(self, source=False, real=False):
+        # Generate images
+        path = os.path.join(self.trainer.save_directory, 'generated_images')
+        os.makedirs(path, exist_ok=True)  # create directory if necessary
+        self.image_count += 1
+        if(source):
+            image = Variable(self.y)
+            image_path = os.path.join(path, 'source.png'.format(self.image_count))
+        elif(real):
+            image = Variable(self.xreal)
+            image_path = os.path.join(path, 'real_target.png'.format(self.image_count))
+        else:
+            image = self.generate()
+            image_path = os.path.join(path, 'target_generated{:08d}.png'.format(self.image_count))
+        # Reshape, scale, and cast the data so it can be saved
+        grid = format_images(image).squeeze(0).permute(1, 2, 0)
+        if grid.size(2) == 1:
+            grid = grid.squeeze(2)
+        array = grid.data.cpu().numpy() * 255.
+        array = array.astype(np.uint8)
+        # Save the image
+        Image.fromarray(array).save(image_path)
+
+
+class CGeneratorTrainingCallback(Callback):
+    # Callback periodically trains the generator
+    def __init__(self, args, parameters, criterion, dataset):
+        self.criterion = criterion
+        self.opt = Adam(parameters, args.generator_lr)
+        self.batch_size = args.batch_size
+        self.count = 0
+        self.frequency = args.generator_frequency
+        self.dataset = dataset
+        self.len = len(self.dataset)
+        self.discriminator_L1lambda = args.discriminator_L1lambda
+
+    def end_of_training_iteration(self, **_):
+        # Each iteration check if it is time to train the generator
+        self.count += 1
+        if self.count > self.frequency:
+            self.train_generator()
+            # TODO : add argument to callback
+            self.count = 0
+
+    def train_generator(self):
+        # Train the generator
+
+        # Calculate yfake
+        random_index = np.random.randint(0, self.len)
+        y = Variable(self.dataset[random_index][1]).unsqueeze(0)
+        xreal = Variable(self.dataset[random_index][0]).unsqueeze(0).cuda()
+        if self.trainer.is_cuda():
+            y = y.cuda()
+        xfake = self.trainer.model.generate(y)
+        yfake = self.trainer.model.y_fake(xfake, y)
+        l1 = self.trainer.model.l1_loss(xreal, xfake)
+        # Calculate loss
+        loss = self.criterion(yfake) + l1 * self.discriminator_L1lambda
+        # Perform update
+        self.opt.zero_grad()
+        loss.backward()
+        self.opt.step()
+
+
+def run(args):
+    dataset = AlderleyWrapper()
+
+    save_args(args)  # save command line to a file for reference
+    train_loader = alderley_cgan_data_loader(args, dataset=dataset)  # get the data
+    model = CGANModel(
+        args,
+        discriminator=CDiscriminatorNetwork(args),
+        generator=UnetUpsample())
+
+    # Build trainer
+    trainer = Trainer(model)
+    trainer.build_criterion(CWGANDiscriminatorLoss(penalty_weight=args.penalty_weight, model=model))
+    trainer.build_optimizer('Adam', model.discriminator.parameters(), lr=args.discriminator_lr)
+
+    trainer.save_every((1, 'epochs'))
+    trainer.save_to_directory(args.save_directory)
+    trainer.set_max_num_epochs(args.epochs)
+    trainer.register_callback(CGenerateDataCallback(args, dataset=dataset))
+    trainer.register_callback(CGeneratorTrainingCallback(
+        args,
+        parameters=model.generator.parameters(),
+        criterion=WGANGeneratorLoss(), dataset=dataset))
+    trainer.bind_loader('train', train_loader, num_inputs=2)
+    # Custom logging configuration so it knows to log our images
+
+    if args.cuda:
+        trainer.cuda()
+
+    # Go!
+    trainer.fit()
+
+    # Generate video from saved images
+
+
+def main(argv):
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch GAN Example')
+
+    # Output directory
+    parser.add_argument('--save-directory', type=str,
+                        default='output/alderley_unetgangp/v1', help='output directory')
+
+    # Configuration
+    parser.add_argument('--batch-size', type=int, default=8,
+                        metavar='N', help='batch size')
+    parser.add_argument('--epochs', type=int, default=250,
+                        metavar='N', help='number of epochs')
+    parser.add_argument('--image-frequency', type=int, default=60,
+                        metavar='N', help='frequency to write images')
+    parser.add_argument('--log-image-frequency', type=int, default=100,
+                        metavar='N', help='frequency to log images')
+    parser.add_argument('--generator-frequency', type=int, default=2,
+                        metavar='N', help='frequency to train generator')
+
+    # Hyperparameters
+    parser.add_argument('--discriminator-lr', type=float, default=3e-4,
+                        metavar='N', help='discriminator learning rate')
+    parser.add_argument('--generator-lr', type=float, default=3e-4,
+                        metavar='N', help='generator learning rate')
+    parser.add_argument('--penalty-weight', type=float, default=20.,
+                        metavar='N', help='gradient penalty weight')
+    parser.add_argument('--discriminator-instancenorm', type=bool,
+                        default=False, metavar='N', help='enable IN')
+    parser.add_argument('--generator-instancenorm', type=bool,
+                        default=True, metavar='N', help='enable IN')
+    parser.add_argument('--discriminator-L1', type=bool,
+                        default=True, metavar='N', help='enable IN')
+    parser.add_argument('--discriminator-L1lambda', type=float,
+                        default=100, metavar='N', help='enable IN')
+
+    # Flags
+    parser.add_argument('--no-cuda', action='store_true',
+                        default=False, help='disables CUDA training')
+    parser.add_argument('--no-ffmpeg', action='store_true',
+                        default=True, help='disables video generation')
+
+    args = parser.parse_args(argv)
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    run(args)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ unet_pub.py
@@ -0,0 +1,90 @@
+import argparse
+import math
+import os
+import sys
+import numpy as np
+from PIL import Image
+
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.optim import Adam
+import torch.nn.functional as F
+
+
+class UnetUpsample(nn.Module):
+
+    def __init__(self):
+        super(UnetUpsample, self).__init__()
+
+        # Input is (N, 3, 256, 512)
+        # No BatchNorm after the first layer
+        self.initial_layer = nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1)  # N, 64, 128, 256
+        self.encoder_activation = nn.LeakyReLU(negative_slope=0.2)
+
+        self.encoder_layers = nn.ModuleList([
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # N, 128, 64, 128
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # N, 256, 32, 64
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 16, 32
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 8, 16
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 4, 8
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 2, 4
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 1, 2
+        ])
+
+        self.encoder_norms = nn.ModuleList([
+            nn.InstanceNorm2d(128),
+            nn.InstanceNorm2d(256),
+            nn.InstanceNorm2d(512),
+            nn.InstanceNorm2d(512),
+            nn.InstanceNorm2d(512),
+            nn.InstanceNorm2d(512),
+            nn.InstanceNorm2d(512),
+        ])
+
+        self.decoder_activation = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+
+        self.decoder_layers = nn.ModuleList([
+            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 2, 4
+            nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 4, 8
+            nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 8, 16
+            nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1),  # N, 512, 16, 32
+            nn.ConvTranspose2d(1024, 256, kernel_size=4, stride=2, padding=1),  # N, 256, 32, 64
+            nn.ConvTranspose2d(512, 128, kernel_size=4, stride=2, padding=1),  # N, 128, 64, 128
+            nn.ConvTranspose2d(256, 64, kernel_size=4, stride=2, padding=1)  # N, 64, 128, 256
+        ])
+
+        self.decoder_norms = nn.ModuleList([
+            nn.InstanceNorm2d(512),
+            nn.InstanceNorm2d(512),
+            nn.InstanceNorm2d(512),
+            nn.InstanceNorm2d(512),
+            nn.InstanceNorm2d(256),
+            nn.InstanceNorm2d(128),
+            nn.InstanceNorm2d(64),
+        ])
+
+        self.final_layer = nn.ConvTranspose2d(
+            128, 3, kernel_size=4, stride=2, padding=1)  # N, 3, 256, 512
+        self.final_activation = nn.Tanh()
+
+    def forward(self, y):
+
+        encoder_states = []
+
+        current_input = self.encoder_activation(self.initial_layer(y))
+        encoder_states.append(current_input)
+
+        for i, layer in enumerate(self.encoder_layers):
+            current_input = self.encoder_activation(self.encoder_norms[i](layer(current_input)))
+            encoder_states.append(current_input)
+
+        for i, layer in enumerate(self.decoder_layers):
+            current_input = self.decoder_activation(
+                self.dropout(self.decoder_norms[i](layer(current_input))))
+            current_input = torch.cat([current_input, encoder_states[-1 - i - 1]], dim=1)
+
+        current_input = self.final_activation(self.final_layer(current_input))
+
+        return current_input
